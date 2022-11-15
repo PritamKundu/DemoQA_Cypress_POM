b810b53f6c5b13b2d46324bdc31551c21ad8564e
PritamKundu <pritamkundu.pk5@gmail.com>
2022-11-16 00:55:41 +0600

    Elements

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     
-
     //baseUrl: 'http://automationpractice.com/',
     baseUrl: 'https://mooleker.moocommerce.no/',
     //baseUrl: 'https://demoqa.com/',
